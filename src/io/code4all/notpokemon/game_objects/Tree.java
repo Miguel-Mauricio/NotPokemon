d5127f9aaa3ad6d203034d45b53b70a67407f16d
joaof <joaosf7@gmail.com>
2023-10-15 22:57:27 +0100

    Added Tree and House classes extended from Solid. Added images to test. Added this to Level so we can test

---
+++ src/io/code4all/notpokemon/game_objects/Tree.java
@@ -0,0 +1,13 @@
+package io.code4all.notpokemon.game_objects;
+
+public class Tree extends Solid{
+    public Tree(int x, int y){
+        setPicture(x, y, "io/code4all/notpokemon/pictures/tree.png");
+        this.picture.draw();
+    }
+
+
+    public String getMessage(){
+        return "IM A ROCK";
+    }
+}

c77c7e05ba2bf75cb34f7258e04bc9074b78d5ad
joaof <joaosf7@gmail.com>
2023-10-18 00:24:02 +0100

    changed pokemon position on Battle Screen

--- src/io/code4all/notpokemon/game_objects/pokemon/PlayerPoke.java
+++ src/io/code4all/notpokemon/game_objects/pokemon/PlayerPoke.java
@@ -1,10 +1,15 @@
 package io.code4all.notpokemon.game_objects.pokemon;
 
+import io.code4all.notpokemon.Game;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class PlayerPoke extends Pokemon{
 
     public PlayerPoke() {
         super("PlayerPoke", 1, 100, 50, 50);
-        setPicture(30, 500, "io/code4all/notpokemon/pictures/shitFly.png");
+        Picture p = new Picture(0,0, "io/code4all/notpokemon/pictures/shitFly.png");
+        p.translate(Game.PADDING, Game.GAME_HEIGHT/2 - p.getHeight()/2);
+        picture = p;
     }
 
     @Override
--- src/io/code4all/notpokemon/game_objects/pokemon/ShitFly.java
+++ src/io/code4all/notpokemon/game_objects/pokemon/ShitFly.java
@@ -1,10 +1,14 @@
 package io.code4all.notpokemon.game_objects.pokemon;
+import io.code4all.notpokemon.Game;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class ShitFly extends Pokemon{
 
     public ShitFly() {
         super("ShitFly", 1, 100, 20, 50);
-        setPicture(730, 30, "io/code4all/notpokemon/pictures/shitFly.png");
+        Picture p = new Picture(0,0, "io/code4all/notpokemon/pictures/shitFly.png");
+        p.translate(Game.GAME_WIDTH - p.getWidth() - Game.PADDING, Game.GAME_HEIGHT/2 - p.getHeight()/2);
+        picture = p;
     }
 
     @Override

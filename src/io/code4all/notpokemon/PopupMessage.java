c28a8b8523a9a03766980f623f803eadb9d611a0
joaof <joaosf7@gmail.com>
2023-10-21 17:50:44 +0100

    removed old textBox

--- src/io/code4all/notpokemon/PopupMessage.java
+++ src/io/code4all/notpokemon/PopupMessage.java
@@ -6,7 +6,6 @@ import org.academiadecodigo.simplegraphics.graphics.Text;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class PopupMessage {
-    private Text textBox;
     private Picture specialAttackBook;
     private Picture normalAttackBook;
     private boolean showing;
@@ -16,28 +15,16 @@ public class PopupMessage {
         this.specialAttackBook.translate(-specialAttackBook.getWidth()/2, -specialAttackBook.getHeight());
         this. normalAttackBook= new Picture(Game.GAME_WIDTH/2 + Game.PADDING, Game.GAME_HEIGHT - Game.PADDING, "io/code4all/notpokemon/pictures/normalAttacksBook.png");
         this.normalAttackBook.translate(-normalAttackBook.getWidth()/2, -normalAttackBook.getHeight());
-        //this.textBox = new Text(board.getX() + 50, board.getY() + 200, "BLA BLA BLA PRESS SPACE");
-        //this.textBox.grow(10, 10);
-    }
-    public Text getTextBox() {
-        return textBox;
-    }
-
-    public void setTextBox(Text textBox) {
-        this.textBox = textBox;
     }
 
     public void disappear() {
         showing = false;
         this.specialAttackBook.delete();
         this.normalAttackBook.delete();
-        this.textBox.delete();
     }
 
     public void show(){
         showing = true;
-        //this.board.draw();
-//        this.textBox.draw();
     }
 
     public boolean showing() {

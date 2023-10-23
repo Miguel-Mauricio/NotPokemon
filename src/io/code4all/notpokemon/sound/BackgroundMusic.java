258cdc815fd08a7a3806265e52cb040e4edb7055
joaof <joaosf7@gmail.com>
2023-10-23 20:45:25 +0100

    removed ant build file and build dir

--- src/io/code4all/notpokemon/sound/BackgroundMusic.java
+++ src/io/code4all/notpokemon/sound/BackgroundMusic.java
@@ -2,17 +2,10 @@ package io.code4all.notpokemon.sound;
 
 import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class BackgroundMusic  {
-
-
         public static void playMusic() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
             InputStream audioSrc = BackgroundMusic.class.getResourceAsStream("/test.wav");
             InputStream bufferedIn = new BufferedInputStream(audioSrc);
@@ -20,8 +13,6 @@ public class BackgroundMusic  {
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
             clip.loop(Clip.LOOP_CONTINUOUSLY);
-
-
     }
 }
 

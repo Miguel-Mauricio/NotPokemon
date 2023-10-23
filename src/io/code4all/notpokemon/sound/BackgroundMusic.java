a80616988cbb9b2119ffb9c92756adbbf222ff85
joaof <joaosf7@gmail.com>
2023-10-23 20:34:23 +0100

    Adds test.wav as resource and BackgroundMusic stops using file class for better compability with jar creation

--- src/io/code4all/notpokemon/sound/BackgroundMusic.java
+++ src/io/code4all/notpokemon/sound/BackgroundMusic.java
@@ -1,17 +1,22 @@
 package io.code4all.notpokemon.sound;
 
 import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class BackgroundMusic  {
 
 
         public static void playMusic() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        File file = new File("src/io/code4all/notpokemon/sound/music/test.wav");
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            InputStream audioSrc = BackgroundMusic.class.getResourceAsStream("/test.wav");
+            InputStream bufferedIn = new BufferedInputStream(audioSrc);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
             clip.loop(Clip.LOOP_CONTINUOUSLY);
